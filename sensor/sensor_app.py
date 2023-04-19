a6ad89e3fc2bcd546f88e8b58db073b479712e00
osgrass <64144934+osgrass@users.noreply.github.com>
2023-04-19 18:10:03 -0400

    module3

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -2,6 +2,7 @@
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import datetime, date
+from temperature_info import TemperatureData
 
 ##############################
 # Do not remove these two lines
@@ -18,6 +19,7 @@ print("Loaded records: {}".format(len(data)))
 house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area("id", rec_area=test_area)
+
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
@@ -25,6 +27,14 @@ recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 3 code here:
 
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Temperature sensor recorders for area {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperature".format(max(recs), min(recs)))
+
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Temperature sensor recorders for date: {} = {}".format(test_date.strftime("%m/%d%y"), len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperature".format(max(recs), min(recs)))
 # Module 4 code here:
 
 # Module 5 code here:
